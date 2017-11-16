fdd5138b6bfccbc279629592cd248156cd9dd9ac
Unknown <tudor.carare@gmail.com>
2017-11-16 21:06:56 +0200

    Add the project

--- BowlingGamePresentation/Game.swift
+++ BowlingGamePresentation/Game.swift
@@ -7,3 +7,51 @@
 //
 
 import Foundation
+
+public class Game {
+    
+    private var rolls: [Int] = []
+    
+    public func roll(_ pins: Int) {
+        rolls.append(pins)
+    }
+
+    public var score: Int {
+        var s = 0
+        var firstInFrame = 0
+        for _ in 0..<10 {
+            if isStrike(firstInFrame) {
+                s += 10 + strikeBonus(firstInFrame)
+                firstInFrame += 1
+            } else if isSpare(firstInFrame) {
+                s += 10 + spareBonus(firstInFrame)
+                firstInFrame += 2
+            } else {
+                s += scoreForFrame(firstInFrame)
+                firstInFrame += 2
+            }
+        }
+        
+        return s
+    }
+
+    fileprivate func scoreForFrame(_ firstInFrame: Int) -> Int {
+        return rolls[firstInFrame] + rolls[firstInFrame+1]
+    }
+    
+    fileprivate func strikeBonus(_ firstInFrame: Int) -> Int {
+        return rolls[firstInFrame+1] + rolls[firstInFrame+2]
+    }
+    
+    fileprivate func spareBonus(_ firstInFrame: Int) -> Int {
+        return rolls[firstInFrame+2]
+    }
+    
+    fileprivate func isSpare(_ firstInFrame: Int) -> Bool {
+        return rolls[firstInFrame] + rolls[firstInFrame+1] == 10
+    }
+
+    fileprivate func isStrike(_ firstInFrame: Int) -> Bool {
+        return rolls[firstInFrame] == 10
+    }
+}
--- BowlingGamePresentationTests/BowlingGamePresentationTests.swift
+++ BowlingGamePresentationTests/BowlingGamePresentationTests.swift
@@ -6,31 +6,78 @@
 //  Copyright © 2017 Tudor Carare. All rights reserved.
 //
 
+/*
+
+ RULES:
+ 1) Write test that fails
+ 2) Write code that will make the tests pass
+ 3) Refactor if needed
+ */
+
 import XCTest
 @testable import BowlingGamePresentation
 
 class BowlingGamePresentationTests: XCTestCase {
     
+    private var g: Game!
+    
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        g = Game()
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        g = nil
         super.tearDown()
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+
+    fileprivate func rollMany(_ n: Int, _ pins: Int) {
+        for _ in 0..<n {
+            g.roll(pins)
         }
     }
+   
+    fileprivate func rollSpare() {
+        g.roll(5)
+        g.roll(5)
+    }
     
+    fileprivate func rollStrike() {
+        g.roll(10)
+    }
+    
+    func testGutterGame() {
+        rollMany(20, 0)
+        
+        XCTAssertEqual(g.score, 0)
+    }
+    
+    func testAllOnes() {
+        rollMany(20, 1)
+        
+        XCTAssertEqual(g.score, 20)
+    }
+    
+    func testOneSpare() {
+        rollSpare()
+        g.roll(3)
+        rollMany(17, 0)
+        
+        XCTAssertEqual(g.score, 16)
+    }
+    
+    func testOneStrike() {
+        rollStrike()
+        g.roll(3)
+        g.roll(4)
+        rollMany(17, 0)
+        
+        XCTAssertEqual(g.score, 24)
+    }
+    
+    func testPerfectGame() {
+        rollMany(12, 10)
+        
+        XCTAssertEqual(g.score, 300)
+    }
 }
